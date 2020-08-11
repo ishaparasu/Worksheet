f2870912f52affd22abd252e6e5f99eed5e967a5
ishaparasu <68916318+ishaparasu@users.noreply.github.com>
2020-08-11 12:40:53 +0530

    Update Counter_function.java

--- Lab_ex_3/Counter_function.java
+++ Lab_ex_3/Counter_function.java
@@ -7,38 +7,37 @@ package Lab_ex_3;
 
 /**
  *
- * @author Baruni Priya T S
+ * @author Isha Parasu B
  */
 public class Counter_function {
     public static void main(String[] args) {
         System.out.println("Without static variable:");
         without_static w=new without_static();
-        without_static w1=new without_static();
-        without_static w2=new without_static();
-        
+        without_static ww=new without_static();
+        without_static wwww=new without_static();
         System.out.println("With static variable:");
-        with_static w3=new with_static();
-        with_static w4=new with_static();
-        with_static w5=new with_static();
+        with_static wwwww=new with_static();
+        with_static wwwwww=new with_static();
+        with_static wwwwwww=new with_static();
     }    
 }
 
 class without_static
 {
-    int num=0;
+    int num=10;
     without_static()
     {
-        num++;//0++ 1
-        System.out.println(num);//1 1 1
+        num=num+10;
+        System.out.println(num);// 20 20 20
     }
 }
 
 class with_static
 {
-    static int num=0;
+    static int num=10;
     with_static()
     {
-        num++;
-        System.out.println(num);//1 2 3
+        num=num+10;
+        System.out.println(num);//20 30 40
     }
 }
