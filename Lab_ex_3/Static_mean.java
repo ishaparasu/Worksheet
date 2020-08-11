2b640d6b07b27f8287689a5d34b37a212edf937b
ishaparasu <68916318+ishaparasu@users.noreply.github.com>
2020-08-11 12:42:36 +0530

    Update Static_mean.java

--- Lab_ex_3/Static_mean.java
+++ Lab_ex_3/Static_mean.java
@@ -7,27 +7,27 @@ package Lab_ex_3;
 import java.util.Scanner;
 /**
  *
- * @author Baruni Priya T S
+ * @author Isha Parasu B
  */
 public class Static_mean {
     public static void main(String[] args) {
         Scanner obj=new Scanner(System.in);
         System.out.println("Enter the size of the array:");
-        int num=obj.nextInt();
-        double array[]=new double[num];
+        int n=obj.nextInt();
+        double a[]=new double[n];
         System.out.println("Enter the elements of the array");
-        for(int i=0;i<num;i++)
-            array[i]=obj.nextDouble();
-        mean(array,num);
+        for(int i=0;i<n;i++)
+            a[i]=obj.nextDouble();
+        mean(a,n);
     }
-    static void mean(double array[],int num)
+    static void mean(double a[],int n)
     {   
-        double sum=0,average=0;
-        for(int i=0;i<num;i++)
+        double sum=0,avg=0;
+        for(int i=0;i<n;i++)
         {
-            sum=sum+array[i];
+            sum=sum+a[i];
         }
-        average=sum/num;
-        System.out.println("Mean of the array:"+average);
+        avg=sum/n;
+        System.out.println("Mean of the array:"+avg);
     }
 }
