458c8c63defd5aa2dbd92f198de992c9f57867f2
ishaparasu <68916318+ishaparasu@users.noreply.github.com>
2020-08-11 12:34:45 +0530

    Update Total_average.java

--- Lab_ex_2/Total_average.java
+++ Lab_ex_2/Total_average.java
@@ -7,71 +7,62 @@ package Lab_ex_2;
 import java.util.Scanner;
 /**
  *
- * @author Baruni Priya T S
+ * @author Isha Parasu B
  */
 public class Total_average {
     public static void main(String[] args) {
         Scanner obj=new Scanner(System.in);
-        Total_average ta=new Total_average();
-        
+        Total_average avg=new Total_average();
         double a[]=new double[5];
         double b[]=new double[5];   
         double c[]=new double[5];
         double d[]=new double[5];
-        double e[]=new double[5];
-           
-        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of student 1:");
+        double e[]=new double[5];           
+        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of 1 st student:");
         for(int i=0;i<=4;i++)
             a[i]=obj.nextDouble();
-        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of student 2:");
+        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of 2 nd student:");
         for(int i=0;i<=4;i++)
             b[i]=obj.nextDouble();
-        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of student 1:");
+        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of 3 rd student:");
         for(int i=0;i<=4;i++)
             c[i]=obj.nextDouble();
-        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of student 1:");
+        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of 4 student:");
         for(int i=0;i<=4;i++)
             d[i]=obj.nextDouble();
-        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of student 1:");
+        System.out.println("Enter 5 subject(ENG,TAM,MAT,SCI,SOC) marks of 5 student:");
         for(int i=0;i<=4;i++)
-            e[i]=obj.nextDouble();
-        
-        list l=new list();
-        l.calc(a,1);
-        l.calc(b,2);
-        l.calc(c,3);
-        l.calc(d,4);
-        l.calc(e,5);
-        
-        ta=null;
+            e[i]=obj.nextDouble();        
+        list li=new list();
+        li.calc(a,1);
+        li.calc(b,2);
+        li.calc(c,3);
+        li.calc(d,4);
+        li.calc(e,5);        
+        avg=null;
         System.gc();//calling destructor
     }
-    
     public void finalize()//Destructor
     {
         System.out.println("Object is destroyed by the Garbage Collector");
-    }
-    
+    }    
 }
 
 class list
 {   
-    double total,average;
-   
+    double tot,avg;
     list() 
     {
-        total=0;
-        average=0;
-    }
-    
-    void calc(double array[],int stud) 
+        tot=0;
+        avg=0;
+    }    
+    void calc(double arr[],int stu) 
     {
         for(int i=0;i<4;i++)
-            total=total+array[i];
+            tot=tot+arr[i];
         average=total/5;
-        System.out.println("Student "+stud+":");
-        System.out.println("Total:"+total);
-        System.out.println("Average:"+average+"\n");
-    }
-   
+        System.out.println("Student "+stu+":");
+        System.out.println("Total:"+tot);
+        System.out.println("Average:"+avg+"\n");
+    }   
 }
