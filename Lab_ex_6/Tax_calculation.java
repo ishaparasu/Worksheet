afbd3b5706d4ceceedfcfab5f40d207e0170c0ec
ishaparasu <68916318+ishaparasu@users.noreply.github.com>
2020-09-01 18:32:48 +0530

    Update Tax_calculation.java

--- Lab_ex_6/Tax_calculation.java
+++ Lab_ex_6/Tax_calculation.java
@@ -7,70 +7,60 @@ package Lab_ex_6;
 import java.util.Scanner;
 /**
  *
- * @author Baruni Priya T S
+ * @author Isha Parasu B
  */
 public class Tax_calculation {
     public static void main(String[] args) {
-        
         Scanner obj=new Scanner(System.in);
-        System.out.println("Enter the gender of the employee:");
-        System.out.println("Press 'm' for male and press 'f' for female:");
+        System.out.println("Enter the gender of the employee(Press 'M' for male and press 'F' for female)");
         String gender=obj.next();
         System.out.println("Enter the income of the employee:");
         double salary=obj.nextDouble();
-   
-        employee1 e=new employee1();
-        e.information(gender,salary);
-        e.show();  
-       
+        employee1 emp=new employee1();
+        emp.information(gender,salary);
+        emp.show();        
     } 
 }
-
-
 interface customer
 {
-    void information(String g,double s);
+    void information(String gen,double sal);
     void show();
 }
-
-
 class employee1 implements customer
 {
-    String gen;
-    double sal;
-    public void information(String g,double s)
+    String g;
+    double s;
+    public void information(String gen,double sal)
     {
-        gen=g;
-        sal=s;
-        
+        g=gen;
+        s=sal; 
     }
-    
     public void show()
     {
-        System.out.println("Gender of the employee:"+gen);
-        System.out.println("Income of the employee:"+sal);
-        
-        if(gen=="m")
+        System.out.println("Employee details:");
+        System.out.println("Gender:"+g);
+        System.out.println("Income"+s);
+        if(g=="M")
         {
-            if(sal<=190000)
-                System.out.println("Tax percentage:NIL");
-            else if(sal>190000 && sal<=200000)
-                System.out.println("Tax percentage:10%");
-            else if(sal>200000 && sal<=500000)
-                System.out.println("Tax percentage:20%");
-            else//if salary>500000
-                System.out.println("Tax percentage:25%");
+            if(s<=190000)
+                System.out.println("Tax percentage is NIL");
+            else if(s>190000 && s<=200000)
+                System.out.println("Tax percentage is 10%");
+            else if(s>200000 && s<=500000)
+                System.out.println("Tax percentageis 20%");
+            else//if s>500000
+                System.out.println("Tax percentage is 25%");
         }            
-        else//if gender=='f'
+        else//if g=='F'
         {
-           if(sal<=190000)
-                System.out.println("Tax percentage:NIL");
-            else if(sal>190000 && sal<=200000)
-                System.out.println("Tax percentage:NIL");
-            else if(sal>200000 && sal<=500000)
-                System.out.println("Tax percentage:10%");
-            else//if salary>500000
-                System.out.println("Tax percentage:20%"); 
+           if(s<=190000)
+                System.out.println("Tax percentage is NIL");
+            else if(s>190000 && s<=200000)
+                System.out.println("Tax percentage is NIL");
+            else if(s>200000 && s<=500000)
+                System.out.println("Tax percentage is 10%");
+            else//if s>500000
+                System.out.println("Tax percentage is 20%"); 
         } 
     }
 }
