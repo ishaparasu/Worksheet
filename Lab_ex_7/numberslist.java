1fe2d244b76dd0b0099e50d8756f24bc540f0a86
ishaparasu <68916318+ishaparasu@users.noreply.github.com>
2020-09-08 14:21:07 +0530

    Update numberslist.java

--- Lab_ex_7/numberslist.java
+++ Lab_ex_7/numberslist.java
@@ -4,47 +4,37 @@
  * and open the template in the editor.
  */
 package Lab_ex_7;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
+
 /**
  *
- * @author Baruni Priya T S
+ * @author Isha Parasu B
  */
 public class numberslist{
     public static void main(String[] args) {
         Scanner obj=new Scanner(System.in);
-        int in;
-        System.out.println("Enter the 10 list elements:");
-        ArrayList<Integer> list=new ArrayList<>(10);//creating an ArrayList
+        int num;
+        double tot=0,avg;
+        System.out.println("Enter the 10 elements:");
+        ArrayList<Integer> list=new ArrayList<>(10);
         for(int i=0;i<10;i++)
         {
-            in=obj.nextInt();//input the elements from the user
-            list.add(in);//adding the elements to the list
+            num=obj.nextInt();
+            list.add(num);
         }
-        /*list.add(1811);
-        list.add(300);
-        list.add(257);
-        list.add(2210);
-        list.add(178);
-        list.add(1907);
-        list.add(2203);
-        list.add(206);
-        list.add(922);
-        list.add(701);*/
-        System.out.println("List:"+list);//printing the list
-        double total=0;
+        System.out.println("List is "+list);
         for(int i=0;i<10;i++)
         {
-            total=total+list.get(i);//finding the sum of all numbers
+            tot=tot+list.get(i);
         }
-        double average;//finding the average of all numbers
-        average=total/10;
-        System.out.println("Average of all numbers in the list:"+average);//printing the average of all numbers
-        System.out.println("Lowest number:"+Collections.min(list));//printing the smallest number
-        System.out.println("Highest number:"+Collections.max(list));//printing the largest number
-        list.removeIf((Integer l)->l%2==0);//filters out all the even numbers(ones place divisble by 2 is same as the entire number divisible by 2)
-        System.out.println("Updated list:"+list);//printing the list
+        average=tot/10;
+        System.out.println("Average is "+average);
+        System.out.println("Smallest number is "+Collections.min(list));
+        System.out.println("Largest number is "+Collections.max(list));
+        list.removeIf((Integer in)->in%2==0);
+        System.out.println("Updated list is "+list);
     }
 }
 
