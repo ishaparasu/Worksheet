c38f67a05cd571feeb82e91b64f194770b4aa100
ishaparasu <68916318+ishaparasu@users.noreply.github.com>
2020-09-08 14:10:41 +0530

    Update empsort.java

--- Lab_ex_7/empsort.java
+++ Lab_ex_7/empsort.java
@@ -5,42 +5,40 @@
  */
 package Lab_ex_7;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Collections;
+import java.util.Comparator;
+
 /**
  *
- * @author Baruni Priya T S
+ * @author Isha Parasu B
  */
 public class empsort {
     public static void main(String[] args) {
-        employee e=new employee("Sonal",40000);//1
+        employee emp=new employee("Sonal",40000);
         ArrayList<employee> list=new ArrayList<>();
-        list.add(e);
-        list.add(new employee("Geminika Muthu",75000));//2
-        list.add(new employee("Dharshini Sree",35000));//3
-        list.add(new employee("Jovita",50000));//4
-        list.add(new employee("Isha Parasu",40000));//5
-        list.add(new employee("Darathi",37000));//6
-        list.add(new employee("Amsavarthan",100000));//7
-        list.add(new employee("Aako",60000));//8
-        list.add(new employee("Ram",37000));//9
-        list.add(new employee("Aravinth Vimal",55000));//10
-        list.add(new employee("Sorna",42000));//11
-        list.add(new employee("Guru Priya Dharshini",35000));//12
-        list.add(new employee("Jeya Roshini",48000));//13
-        list.add(new employee("Sathya",33000));//14
-        list.add(new employee("Naseeha Shyma",45000));//15
-        list.add(new employee("Bhavadharani",52000));//16
-        list.add(new employee("Nithila Pallete",70000));//17
-        list.add(new employee("Divya Sri",44000));//18
-        list.add(new employee("Raji Santhoshi",62000));//19
-        list.add(new employee("Jane Nivethitha",72000));//20
-        System.out.println("List:"+list);
-        Collections.sort(list,new namesort());
-        System.out.println("**********************************");
-        System.out.println("Sorted list in descending order of names:");
-        System.out.println(list);       
-        
+        list.add(emp);
+        list.add(new employee("Geminika Muthu",75000));
+        list.add(new employee("Dharshini Sree",35000));
+        list.add(new employee("Jovita",50000));
+        list.add(new employee("Isha Parasu",40000));
+        list.add(new employee("Darathi",37000));
+        list.add(new employee("Amsavarthan",100000));
+        list.add(new employee("Aako",60000));
+        list.add(new employee("Ram",37000));
+        list.add(new employee("Aravinth Vimal",55000));
+        list.add(new employee("Sorna",42000));
+        list.add(new employee("Guru Priya Dharshini",35000));
+        list.add(new employee("Jeya Roshini",48000));
+        list.add(new employee("Sathya",33000));
+        list.add(new employee("Naseeha Shyma",45000));
+        list.add(new employee("Bhavadharani",52000));
+        list.add(new employee("Nithila Pallete",70000));
+        list.add(new employee("Divya Sri",44000));
+        list.add(new employee("Raji Santhoshi",62000));
+        list.add(new employee("Jane Nivethitha",72000));
+        System.out.println("List is "+list);
+        Collections.sort(list,new sort());
+        System.out.println("Sorted list is \n"+list);
     }
     
 }
@@ -48,19 +46,19 @@ public class empsort {
 class employee
 {
     String name;
-    double salary;
-    employee(String n,double s)
+    double sal;
+    employee(String s1,double s2)
     {
-        name=n;
-        salary=s;
+        name=s1;
+        sal=s2;
     }
     public String toString()
     {
-        return "\nName:"+name+"\tSalary:Rs."+salary;
+        return "\nName:"+name+"\tSalary:Rs."+sal;
     }
 }
 
-class namesort implements Comparator<employee>
+class sort implements Comparator<employee>
 {
     public int compare(employee e1,employee e2)
     {
