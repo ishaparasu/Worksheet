a492829e079af3acd09c6d20fd273bb7eeeb5524
ishaparasu <68916318+ishaparasu@users.noreply.github.com>
2020-10-16 15:23:54 +0530

    Add files via upload

---
+++ lab_ex12/Generic_function.java
@@ -0,0 +1,77 @@
+/*
+2.Write a java program to search an element from the given type of elements using a generic function.
+ */
+package lab_ex12;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sonal L R
+ */
+public class Generic_function {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner obj=new Scanner(System.in);
+        Integer[] numbers={20,4,98,30,73,66,87}; 
+        String[] names={"Isha","Kanisha","Yogi","Kamalika","Divya","Deepthi"};
+        Character[] alphabets={'a','b','c','d','e'};
+        Double[] dnumbers={100.00,234.56,34.78,3253.000,743.4,63.56};
+        Float[] fnumbers={21.2f,45.4f,63.5f,5912.7f};
+        
+        search s=new search();
+        System.out.println("Available types: \n1>Integer \n2>String \n3>Character \n4>Double \n5>Float");
+        System.out.println("Press 1 for Integer, 2 for String, 3 for Charcater, 4 for Double and 5 for Float types:");
+        int option=obj.nextInt();
+        switch(option)
+        {
+            case 1:
+                System.out.println("Enter the number to be searched:");
+                int value1=obj.nextInt();
+                s.search(value1,numbers);
+                break;
+            case 2:
+                System.out.println("Enter the name to be searched:");
+                String value2=obj.next();
+                s.search(value2,names);
+                break;
+            case 3:
+                System.out.println("Enter the alphabet to be searched:");
+                char value3=obj.next().charAt(0);
+                s.search(value3,alphabets);
+                break;
+            case 4:
+                System.out.println("Enter the number to be searched:");
+                double value4=obj.nextDouble();
+                s.search(value4,dnumbers);
+                break;
+            case 5:
+                System.out.println("Enter the number to be searched:");
+                float value5=obj.nextFloat();
+                s.search(value5,fnumbers);
+                break;
+        }
+    }
+}
+class search
+{
+    boolean flag=false;
+    public<I,T> void search(I s,T[] array)
+    {
+        for(int i=0;i<array.length;i++)
+        {
+            if(s.equals(array[i])) 
+            {
+                flag=true;
+                break;
+            }
+        }
+        if(flag)
+            System.out.println("'"+s+"' is found in the array.");
+        else
+            System.out.println("'"+s+"' is not found in the array.");
+    }
+}
---
+++ lab_ex12/Generic_method.java
@@ -0,0 +1,88 @@
+/*
+1.Write java programs that include generic method to satisfy the following property.
+    a.	To counts the number of odd integers in an integer list
+    b.	To exchange the positions of two different elements in an array.
+    c.	To find the maximal element in the range [begin, end] of a list.
+ */
+package lab_ex12;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sonal L R
+ */
+public class Generic_method {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner obj=new Scanner(System.in);
+        Integer s[]={33,436,234,65,86,235,7,12};
+        System.out.println("The Integer list elements are:");
+        for(int i=0;i<s.length;i++)
+            System.out.println(s[i]+" ");
+        Collections c=new Collections();
+        c.OddNUmbers(s);
+        
+        System.out.println("Enter the position of 1st element to be exchanged:"); //(1-8)
+        int p1=obj.nextInt();
+        System.out.println("Enter the posiion of 2nd element to be exchanged:"); //(1-8)
+        int p2=obj.nextInt();
+        if(p1>9||p1<0||p2>9||p2<0)
+        {
+            System.out.println("Invalid position!");
+            return;
+        }
+        c.ExchangePOs(s,p1,p2);
+        
+        System.out.println("Enter the begin index to find the maximal element in the specified range:"); //(0-7)
+        int begin=obj.nextInt();
+        System.out.println("Enter the end index to find the maximal element in the specified range:"); //(0-7)
+        int end=obj.nextInt();
+        if(begin>end)
+        {
+            System.out.println("Invalid indices!");
+            return;
+        }
+        c.MaxElement(s,begin,end);
+    }
+}
+class Collections
+{
+    public<I extends Integer> void OddNUmbers(I[] value)
+    {
+        int count=0;
+        for(int i=0;i<value.length;i++)
+        {
+            if(value[i]%2!=0)
+                count++;
+        }
+        if(count>0)
+            System.out.println("Number of odd integers is:"+count);
+        else
+            System.out.println("No odd integers!");
+    }
+    public<I extends Integer> void ExchangePOs(I[] value,int p1,int p2)
+    {
+        I a; 
+        a=value[p1-1];
+        value[p1-1]=value[p2-1];
+        value[p2-1]=a;
+        System.out.println("The list elements after exchanging are:");
+        for(int i=0;i<value.length;i++)
+            System.out.println(value[i]+" ");
+    }
+    public<A extends Integer> void MaxElement(A[] value,int begin,int end)
+    {
+        A max=value[begin];
+        for(int i=begin;i<=end;i++)
+        {
+            if(value[i]>max)
+            {
+                max=value[i];
+            }
+        }
+        System.out.println("Maximal element is:"+max);
+    }
+}
